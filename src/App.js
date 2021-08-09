e5b04153c7a90a602cf35db2907a56ee1a6facb3
kavandalal003@gmail.com <kavandalal003@gmail.com>
2021-08-09 22:25:34 +0530

    1st delploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BsArrowReturnRight } from 'react-icons/bs';
+import ReactLoading from 'react-loading';
+const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loading, setLoading] = useState(true);
+	const [jobs, setJobs] = useState([]);
+	const [value, setValue] = useState(0);
+	const getPeople = async () => {
+		const res = await fetch(url);
+		const newJobs = await res.json();
+		setJobs(newJobs);
+		setLoading(false);
+	};
+	useEffect(() => {
+		getPeople();
+	}, []);
+	if (loading) {
+		return <>Loading</>;
+	}
+	const { title, dates, duties, company } = jobs[value];
+	return (
+		<section class='page'>
+			<div className='top'>
+				<h1>Tabs</h1>
+				<div className='line'></div>
+			</div>
+			<div className='main'>
+				<section className='btn'>
+					{jobs.map((job, index) => {
+						return (
+							<button
+								key={job.id}
+								onClick={() => {
+									setValue(index);
+								}}
+								className={`job-btn ${index === value && 'active'}`}>
+								{job.company}
+							</button>
+						);
+					})}
+				</section>
+				<section className='desc'>
+					<h3>{title}</h3>
+					<h4>{company}</h4>
+					<h5>{dates}</h5>
+					<article>
+						{duties.map((duty, index) => {
+							return (
+								<div className='para' key={index}>
+									<BsArrowReturnRight />
+									<p>{duty}</p>
+								</div>
+							);
+						})}
+					</article>
+				</section>
+			</div>
+		</section>
+	);
 }
 
 export default App;
